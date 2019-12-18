e9f368de5b35c874279db7b5701dbe5ac2977503
Kevin Lin <32544867+KevinJianLin@users.noreply.github.com>
2019-12-17 22:44:59 -0500

    change

---
+++ Assignment/3/transcriptome.R
@@ -0,0 +1,20 @@
+
+
+gene_list <- read.csv("/Users/Kevin/Documents/MENG/Courses/UoG BINF6410 bio programming/Assignment/3/genes.csv")
+
+
+for i in length(gene_list[2])
+
+use lapply or grep to remove empty rows
+
+
+{
+
+if (gene_list[2]=="null")
+{
+  
+  
+}
+  
+
+gene_list_new <- data.frame()
