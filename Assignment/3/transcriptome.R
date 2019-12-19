ded15746f4aa8af08907047c7a66d6e51b328e8d
Kevin Lin <klin@luminexcorp.com>
2019-12-18 20:56:47 -0500

    gg

--- Assignment/3/transcriptome.R
+++ Assignment/3/transcriptome.R
@@ -2,6 +2,18 @@
 
 gene_list <- read.csv("/Users/Kevin/Documents/MENG/Courses/UoG BINF6410 bio programming/Assignment/3/genes.csv")
 
+genome <- read.csv("/Users/Kevin/Documents/MENG/Courses/UoG BINF6410 bio programming/Assignment/3/eh.fa")
+
+gene_list <- read.csv("/Users/Kevin/Documents/MENG/Courses/UoG BINF6410 bio programming/Assignment/3/eh.gff3")
+
+
+
+
+gene_list <- read.csv("C:/Users/klin/Desktop/KL/Training/Master/Courses/UoG-BINF6410-Bio-Programming/Assignment/3/genes.csv")
+
+genome <- read.csv("C:/Users/klin/Desktop/KL/Training/Master/Courses/UoG-BINF6410-Bio-Programming/Assignment/3/eh.fa")
+
+gene_description <- read.csv("C:/Users/klin/Desktop/KL/Training/Master/Courses/UoG-BINF6410-Bio-Programming/Assignment/3/eh.gff3")
 
 for i in length(gene_list[2])
 
@@ -18,3 +30,9 @@ if (gene_list[2]=="null")
   
 
 gene_list_new <- data.frame()
+
+
+kmer <- c("genename","AA", "AT", "AC","AG","TA","TT","TC","TG","CA","CC","CT","CG","GA","GT","GC","GG")
+
+
+
