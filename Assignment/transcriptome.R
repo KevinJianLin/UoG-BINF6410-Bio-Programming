de6ca125c093a811e1e27db77ad021f84cc4cdda
Kevin Lin <32544867+KevinJianLin@users.noreply.github.com>
2019-12-22 16:31:53 -0500

    fff

--- Assignment/transcriptome.R
+++ Assignment/transcriptome.R
@@ -1,38 +1,46 @@
 
 
-gene_list <- read.csv("/Users/Kevin/Documents/MENG/Courses/UoG BINF6410 bio programming/Assignment/3/genes.csv")
+gene_list <- read.csv("/Users/Kevin/Documents/MENG/Courses/UoG BINF6410 bio programming/Assignment/genes.csv",stringsAsFactors = FALSE)
 
-genome <- read.csv("/Users/Kevin/Documents/MENG/Courses/UoG BINF6410 bio programming/Assignment/3/eh.fa")
+genome <- read.csv("/Users/Kevin/Documents/MENG/Courses/UoG BINF6410 bio programming/Assignment/eh.fa")
 
-gene_list <- read.csv("/Users/Kevin/Documents/MENG/Courses/UoG BINF6410 bio programming/Assignment/3/eh.gff3")
-
-
+gene_list <- read.csv("/Users/Kevin/Documents/MENG/Courses/UoG BINF6410 bio programming/Assignment/eh.gff3")
 
 
-gene_list <- read.csv("C:/Users/klin/Desktop/KL/Training/Master/Courses/UoG-BINF6410-Bio-Programming/Assignment/3/genes.csv")
-
-genome <- read.csv("C:/Users/klin/Desktop/KL/Training/Master/Courses/UoG-BINF6410-Bio-Programming/Assignment/3/eh.fa")
-
-gene_description <- read.csv("C:/Users/klin/Desktop/KL/Training/Master/Courses/UoG-BINF6410-Bio-Programming/Assignment/3/eh.gff3")
-
-for i in length(gene_list[2])
-
-use lapply or grep to remove empty rows
 
 
+gene_list <- read.csv("C:/Users/klin/Desktop/KL/Training/Master/Courses/UoG-BINF6410-Bio-Programming/Assignment/genes.csv")
+
+genome <- read.csv("C:/Users/klin/Desktop/KL/Training/Master/Courses/UoG-BINF6410-Bio-Programming/Assignment/eh.fa")
+
+gene_description <- read.csv("C:/Users/klin/Desktop/KL/Training/Master/Courses/UoG-BINF6410-Bio-Programming/Assignment/eh.gff3")
+
+treatment_type_final <- c()
+gene_list_final <- c()
+
+
+for (i in seq(2:ncol(gene_list)))
 {
 
-if (gene_list[2]=="null")
-{
-  
-  
+a <- as.character(unlist(gene_list[i]))
+
+a >- 
+    
+treatment_type <- rep(colnames(gene_list[i]),length(unlist(gene_list[i])))
+
+treatment_type_final <- c(treatment_type,treatment_type_final)
+
+gene_list_final <-c(gene_list_final,unlist(gene_list[i]))
+
 }
-  
-
-gene_list_new <- data.frame()
 
 
-kmer <- c("genename","AA", "AT", "AC","AG","TA","TT","TC","TG","CA","CC","CT","CG","GA","GT","GC","GG")
+a <- as.character(unlist(gene_list[2]))
+
+
+kmer <- c("genename","AA", "AT", "AC","AG","TA","TT","TC","TG","CA","CC","CT","CG","GA","GT","GC","GG","treatment")
+
+
 
 
 
